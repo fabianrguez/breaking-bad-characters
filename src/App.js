e07c57a37cab2c9729dd69fde451249512abdae3
fabianrguez <fabian.rguezg@gmail.com>
2022-04-06 17:56:51 +0100

    add react router dom and show characters list

--- src/App.js
+++ src/App.js
@@ -1,21 +1,27 @@
 import { useTranslation } from 'react-i18next';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import { getCharacters } from 'services/getCharacters';
 
 function App() {
+  const [characters, setCharacters] = useState([]);
   const { t } = useTranslation();
 
   useEffect(() => {
     (async () => {
-      const characters = await getCharacters();
-      console.log({ characters });
+      const _characters = await getCharacters();
+      setCharacters(_characters);
     })();
   }, []);
 
   return (
     <div className="App">
       <h1>{t('home.title')}</h1>
+      <ul style={{ paddingLeft: '2rem' }}>
+        {characters?.map(({ char_id, name }) => (
+          <li key={char_id}>{name}</li>
+        ))}
+      </ul>
     </div>
   );
 }
