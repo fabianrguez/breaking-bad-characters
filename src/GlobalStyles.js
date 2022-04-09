4c956f28fb2a4d1176d895ad59c6d1d549b2a4b8
fabianrguez <fabian.rguezg@gmail.com>
2022-04-09 18:09:21 +0100

    preload font assets

--- src/GlobalStyles.js
+++ src/GlobalStyles.js
@@ -1,24 +1,6 @@
 import { createGlobalStyle } from 'styled-components';
-import { GrapeNutsRegular, PoppinsBold, PoppinsRegular } from 'assets/fonts';
 
 export const GlobalStyles = createGlobalStyle`
-  @font-face {
-     font-family: 'Grape Nuts';
-     src: url(${GrapeNutsRegular}) format('truetype');
-  }
-  @font-face {
-     font-family: 'Poppins';
-     src: url(${PoppinsRegular}) format('truetype');
-     font-weight: 400;
-     font-style: regular;
-  }
-  @font-face {
-     font-family: 'Poppins';
-     src: url(${PoppinsBold}) format('truetype');
-     font-weight: 600;
-     font-style: bold;
-  }
-
   * {
     margin: 0;
     padding: 0;
--- src/assets/fonts/index.js
+++
@@ -1,5 +0,0 @@
-import GrapeNutsRegular from './GrapeNuts-Regular.ttf';
-import PoppinsBold from './Poppins-Bold.ttf';
-import PoppinsRegular from './Poppins-Regular.ttf';
-
-export { GrapeNutsRegular, PoppinsRegular, PoppinsBold };
